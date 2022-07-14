4c67564d89a5ae625ff265c3e9423c7f5c33f9e0
dskl98dskl <izzudinjundullah@gmail.com>
2022-07-14 23:46:09 +0700

    menambahkan h1 pada file index.php

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <title>Hello Madefaker</title>
 </head>
 <body>
+    <h1>test doang sih </h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A necessitatibus, perspiciatis nobis alias, dignissimos voluptas officiis quae, debitis doloremque velit quibusdam illo sed ipsam distinctio nostrum nam totam fugiat laboriosam!
 </p>
 </body>
